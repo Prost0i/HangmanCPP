060d5db347355271ea9d6cdc19a44b4765def9f2
Mikhail Stasyuk <mihail.stasyuk2014@yandex.ru>
2022-04-02 19:38:01 +0500

    Implemented winning and losing

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
-#include "hangman_status.h"
+#include "hangman_frames.h"
 #include "secret_word.h"
 #include "console.h"
+
+#include <thread>
+#include <chrono>
+
+#define ARRAY_SIZE(arr) (sizeof(arr)/sizeof(arr[0]))
+
+const int ATTEMPTS_COUNT = 6;
+static_assert(ATTEMPTS_COUNT+1 == ARRAY_SIZE(hangman_frames));
 
 void main_loop(Console *console) {
 	std::string frame;
@@ -9,8 +17,8 @@ void main_loop(Console *console) {
 	Console::Key key_type = Console::Key::Unknown;
 	char key_char = 0;
 
-	HangmanStatus hstatus;
 	SecretWord sword("hello");
+	int attempts = 0;
 
 	bool run = true;
 	while (run) {
@@ -25,12 +33,12 @@ void main_loop(Console *console) {
 				run = false;
 			}
 		} else {
-			if(!sword.guess_char(key_char)) {
-				hstatus.increase_counter();
+			if(!sword.guess_char(key_char) && attempts < 6) {
+				++attempts;
 			}
 		}
 
-		frame.append(hstatus.return_state());
+		frame.append(hangman_frames[attempts]);
 		frame.append("\r\n\r\n");
 		frame.append(sword.return_word_status());
 		frame.append("\r\n\r\n");
@@ -41,7 +49,21 @@ void main_loop(Console *console) {
 		console->write(frame.c_str());
 
 		if (sword.is_all_chars_are_guessed()) {
+			frame.append("\r\nYou win!!!\r\n");
+			console->clear();
+			console->write(frame.c_str());
+
 			run = false;
+			std::this_thread::sleep_for(std::chrono::seconds(1));
+		}
+
+		if (attempts >= ATTEMPTS_COUNT) {
+			frame.append("\r\nYou lose!!!\r\n");
+			console->clear();
+			console->write(frame.c_str());
+
+			run = false;
+			std::this_thread::sleep_for(std::chrono::seconds(1));
 		}
 	}
 }
